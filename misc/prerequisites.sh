234c09d54ffd9b51b37a79bc00ec18644ce206b5
Yuri Pereira <yuri.pereira@aglets.com.br>
2025-10-18 15:02:25 -0300

    fixset

--- misc/prerequisites.sh
+++ misc/prerequisites.sh
@@ -263,10 +263,9 @@ install_ffmpeg()
     # If there is an enable-nvc option, add nvcodec
     if [ "$NVIDIA_NV_CODEC_HWACCELS" = true ] ; then
         ADDI_CFLAGS+="-I/usr/local/cuda/include "
-        ADDI_CFLAGS+="-gencode arch=compute_52,code=sm_52 "
         ADDI_LDFLAGS="-L/usr/local/cuda/lib64 "
         ADDI_LICENSE+=" --enable-nonfree "
-        ADDI_LIBS+=" --enable-cuda-nvcc --enable-cuda-llvm --enable-nvenc --enable-nvdec --enable-ffnvcodec --enable-cuvid "
+        ADDI_LIBS+=" --enable-cuda-nvcc --enable-nvenc --enable-nvdec --enable-ffnvcodec --enable-cuvid "
         ADDI_HWACCEL="--enable-hwaccel=cuda,cuvid "
         ADDI_ENCODER+=",h264_nvenc,hevc_nvenc"
         ADDI_DECODER+=",h264_nvdec,hevc_nvdec,h264_cuvid,hevc_cuvid"
