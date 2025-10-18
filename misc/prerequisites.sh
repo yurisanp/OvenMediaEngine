2824ee5c398023717778187e0df879401814fc04
Yuri Pereira <yuri.pereira@aglets.com.br>
2025-10-18 16:35:25 -0300

    fisted

--- misc/prerequisites.sh
+++ misc/prerequisites.sh
@@ -262,17 +262,15 @@ install_ffmpeg()
 
     # If there is an enable-nvc option, add nvcodec
     if [ "$NVIDIA_NV_CODEC_HWACCELS" = true ] ; then
-        ADDI_CFLAGS+="-I/usr/local/cuda/include "
-        ADDI_CFLAGS+="-gencode arch=compute_52,code=sm_52 "
+        ADDI_CFLAGS+="-I/usr/local/cuda/include -gencode arch=compute_52,code=sm_52 "
         ADDI_LDFLAGS="-L/usr/local/cuda/lib64 "
         ADDI_LICENSE+=" --enable-nonfree "
-        ADDI_LIBS+=" --enable-cuda-nvcc --enable-cuda-llvm --enable-nvenc --enable-nvdec --enable-ffnvcodec --enable-cuvid "
+        ADDI_LIBS+=" --enable-cuda-nvcc --disable-cuda-llvm --enable-nvenc --enable-nvdec --enable-ffnvcodec --enable-cuvid "
         ADDI_HWACCEL="--enable-hwaccel=cuda,cuvid "
         ADDI_ENCODER+=",h264_nvenc,hevc_nvenc"
         ADDI_DECODER+=",h264_nvdec,hevc_nvdec,h264_cuvid,hevc_cuvid"
         ADDI_FILTERS+=",scale_cuda,hwdownload,hwupload,hwupload_cuda"
-
-        PATH=$PATH:/usr/local/nvidia/bin:/usr/local/cuda/bin
+        PATH=/usr/local/cuda/bin:/usr/local/nvidia/bin:$PATH
     fi
 
     if [ "$VIDEOLAN_X264_CODEC" == true ]; then
