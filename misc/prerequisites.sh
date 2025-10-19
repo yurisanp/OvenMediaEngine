8a09bb63da7fa619d1abf070ec6139e67ac5c6b3
Yuri Pereira <yuri.pereira@aglets.com.br>
2025-10-18 23:44:49 -0300

    remove optimize bean

--- misc/prerequisites.sh
+++ misc/prerequisites.sh
@@ -265,7 +265,7 @@ install_ffmpeg()
         ADDI_CFLAGS+="-I/usr/local/cuda/include "
         ADDI_LDFLAGS="-L/usr/local/cuda/lib64 "
         ADDI_LICENSE+=" --enable-nonfree "
-        ADDI_LIBS+=" --enable-cuda-nvcc --enable-libnpp --enable-nvenc --enable-nvdec --enable-ffnvcodec --enable-cuvid --nvccflags=\"-gencode arch=compute_52,code=sm_52 -O2\" "
+        ADDI_LIBS+=" --enable-cuda-nvcc --enable-libnpp --enable-nvenc --enable-nvdec --enable-ffnvcodec --enable-cuvid --nvccflags=\"-gencode arch=compute_52,code=sm_52\" "
         ADDI_HWACCEL="--enable-hwaccel=cuda,cuvid "
         ADDI_ENCODER+=",h264_nvenc,hevc_nvenc"
         ADDI_DECODER+=",h264_nvdec,hevc_nvdec,h264_cuvid,hevc_cuvid"
